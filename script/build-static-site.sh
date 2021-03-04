68c18a535dca3c2ea9e0b0db12827b279f9f83f0
Nathan Moos <moosingin3space@gmail.com>
2021-03-03 20:31:09 -0800

    fix build-static-site script

--- script/build-static-site.sh
+++ script/build-static-site.sh
@@ -3,4 +3,6 @@
 # This script is intended to be invoked from the root of the project. While
 # today, it is trivial, it may grow.
 
-(cd client && npx gatsby build)
+cd client
+npm install
+npx gatsby build
